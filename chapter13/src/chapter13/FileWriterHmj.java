def0a9eff4327d9f37d244fef3c89590edbd2545
HMJda <hmj4669@gmail.com>
2022-11-28 18:55:17 +0900

    .

--- chapter13/src/chapter13/FileWriterHmj.java
+++ chapter13/src/chapter13/FileWriterHmj.java
@@ -1,10 +1,24 @@
 package chapter13;
+import java.io.*; 
 
 public class FileWriterHmj {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		InputStreamReader in = new InputStreamReader(System.in);
+		
+		
+		FileWriter fout = null;
+		int c;
+		try {
+			fout = new FileWriter(args[0]);
+			while((c = in.read()) != -1) {
+				fout.write(c);
+			}
+			in.close();
+			fout.close();
+		}catch(IOException e) {
+			System.out.println("입출력 오류");
+		}
 	}
 
 }
